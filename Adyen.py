6e941bd8be721667e9bdfb0c30ce39ecf63f8202
Ehit <bongoehit@gmail.com>
2023-04-22 19:52:06 +0200

    Version 1

---
+++ Adyen.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template,jsonify, request, url_for
+import requests
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/handle', methods=['POST'])
+
+def addRegion():
+    AM = float(request.form['AM'])*100
+    CA = request.form['CA']
+    IN = request.form['I']
+
+    # Following is for generating payment reference
+    currentDateAndTime = datetime.now()
+    Date = str(currentDateAndTime)
+    string_element = ''.join([char for char in Date if char.isnumeric()])
+
+    Currency = { "SG":"SGD", "GB":"GBP", "US":"USD", "NL":"EUR", "AU":"AUD"}
+
+    Headers = {"x-API-key": "AQEhhmfuXNWTK0Qc+iSGnWssruqDEIseWIfhSMkKMcCEcDinEMFdWw2+5HzctViMSCJMYAc=-/irxwsKh3s196odKGl0ZNVUylzfzCq1tmbcx5MwStZE=-J]~^%8KbK*fVz^Dw", "content-type": "application/json"}
+
+    body = { "merchantAccount": "VootinECOM",
+         "amount": { "value": AM,"currency": Currency[CA] },
+         "returnUrl": "http://127.0.0.1:5000/redirect",
+         "reference": 'Ref_'+string_element,
+         "countryCode": CA
+         }
+
+    Result = requests.post("https://checkout-test.adyen.com/v69/sessions",headers = Headers,json = body)     
+    #print(f"Received project filepath: {project_filepath}")
+    Result = Result.json()
+    print(IN)
+    #return Result["sessionData"]
+    if(IN == 'W'):
+    	return render_template('Web Drop-In.html', Sessions= Result["sessionData"], ID = Result["id"])
+    elif(IN == 'C'):
+    	return render_template('Web Components.html', Sessions= Result["sessionData"], ID = Result["id"])
+    else:
+    	return render_template('gg.html')
+
+@app.route('/redirect', methods=['GET'])
+
+def status():
+    sessionId = request.args.get('sessionId')
+    redirectResult = request.args.get('redirectResult')
+
+    headers = {"x-API-key": "AQEhhmfuXNWTK0Qc+iSGnWssruqDEIseWIfhSMkKMcCEcDinEMFdWw2+5HzctViMSCJMYAc=-/irxwsKh3s196odKGl0ZNVUylzfzCq1tmbcx5MwStZE=-J]~^%8KbK*fVz^Dw", "content-type": "application/json"}
+    body =  { "details": {"redirectResult":redirectResult}}
+
+    Result = requests.post("https://checkout-test.adyen.com/v69/payments/details",headers = headers, json = body)
+    Result = Result.json()
+
+    return render_template('redirect.html', Outcome = Result['resultCode'], Reference = Result['merchantReference'], Amount = Result['amount']['value'], Currency =Result['amount']['currency']  )
+
+@app.route('/')
+
+def func():
+
+	return render_template("Homepage.html")
+
+@app.route('/checkout', methods=['POST'])
+
+def checkout():
+    header2 = request.headers.get('amount')
+    #projectpath = request.form['myForm']
+    return render_template('checkout.html', am=header2)
+
+
+if __name__ == "__main__":
+    app.run()
